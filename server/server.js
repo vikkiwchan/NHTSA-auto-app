f818157f069d788b7e10858b617ced475558b4d1
Vikki Chan <vikkiwchan@gmail.com>
2021-07-29 11:07:27 -0500

    first commit

--- server/server.js
+++ server/server.js
@@ -4,9 +4,10 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 
 // Static Files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.listen(port, () => {
