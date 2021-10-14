5423c173120dbd01dcfeb95976b81fac4e3e24da
vikki <vchan@DESKTOP-I9LRHSV.localdomain>
2021-10-13 19:10:46 -0500

    test commit

--- client/Components/Form.jsx
+++ client/Components/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Form = ({ setResults }) => {
+  console.log('Hi');
   const [year, setYear] = useState('');
   const [make, setMake] = useState('');
   const [model, setModel] = useState('');
